66c8607d7fc9a2141a80103809a719981d49f3c5
Nikolay Beletskiy <nikolay0beletskiy@gmail.com>
2023-10-28 14:18:18 +0300

    created database and table "exams"

---
+++ database.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+
+class Exams:
+    @staticmethod
+    def create(subject, date):
+        with sqlite3.connect('commission.db') as db:
+            cursor = db.cursor()
+            cursor.execute(f"INSERT INTO exams (subject, date) VALUES {(subject, date)}")
+            db.commit()
+
+    @staticmethod
+    def read(id):
+        with sqlite3.connect('commission.db') as db:
+            cursor = db.cursor()
+            cursor.execute(f"SELECT * FROM exams WHERE rowid = {id}")
+            return cursor.fetchone()
+
+    @staticmethod
+    def read_all():
+        with sqlite3.connect('commission.db') as db:
+            cursor = db.cursor()
+            cursor.execute(f"SELECT * FROM exams")
+            return cursor.fetchall()
+
+    @staticmethod
+    def delete(id):
+        with sqlite3.connect('commission.db') as db:
+            cursor = db.cursor()
+            cursor.execute(f"DELETE FROM exams WHERE rowid = {id}")
+            db.commit()
