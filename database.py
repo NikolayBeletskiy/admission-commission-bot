bcc1c0701eb1e649e254a8079abd16fa5523f0a2
Nikolay Beletskiy <nikolay0beletskiy@gmail.com>
2023-10-31 17:05:44 +0300

    created 2 tables and commands

--- database.py
+++ database.py
@@ -1,31 +1,84 @@
 import sqlite3
 
 
+class DataBase:
+    def __init__(self, database: str):
+        self.database = database
+
+    def __do(self, command: str):
+        with sqlite3.connect(self.database) as db:
+            cursor = db.cursor()
+            cursor.execute(command)
+            db.commit()
+            return cursor
+
+    def execute(self, command: str):
+        self.__do(command)
+
+    def select(self, command):
+        return self.__do(command).fetchall()
+
+    def select_one(self, command):
+        return self.__do(command).fetchone()
+
+
+db = DataBase('commission.db')
+
+
 class Exams:
     @staticmethod
     def create(subject, date):
-        with sqlite3.connect('commission.db') as db:
-            cursor = db.cursor()
-            cursor.execute(f"INSERT INTO exams (subject, date) VALUES {(subject, date)}")
-            db.commit()
+        db.execute(f"INSERT INTO exams (subject, date) VALUES {(subject, date)}")
 
     @staticmethod
     def read(id) -> tuple:
-        with sqlite3.connect('commission.db') as db:
-            cursor = db.cursor()
-            cursor.execute(f"SELECT * FROM exams WHERE rowid = {id}")
-            return cursor.fetchone()
+        return db.select_one(f"SELECT * FROM exams WHERE rowid = {id}")
 
     @staticmethod
-    def read_all() ->list[tuple]:
-        with sqlite3.connect('commission.db') as db:
-            cursor = db.cursor()
-            cursor.execute(f"SELECT * FROM exams")
-            return cursor.fetchall()
+    def read_all() -> list[tuple]:
+        return db.select(f"SELECT * FROM exams")
 
     @staticmethod
     def delete(id):
-        with sqlite3.connect('commission.db') as db:
-            cursor = db.cursor()
-            cursor.execute(f"DELETE FROM exams WHERE rowid = {id}")
-            db.commit()
+        db.execute(f"DELETE FROM exams WHERE rowid = {id}")
+
+
+class Specialties:
+    @staticmethod
+    def create(id, name: str, subject1: str, subject2: str, subject3: str):
+        values = (id, name, subject1, subject2, subject3)
+        db.execute(f"INSERT INTO specialties (id, name, subject1, subject2, subject3) VALUES {values}")
+
+    @staticmethod
+    def read(id) -> tuple:
+        return db.select_one(f"SELECT * FROM specialties WHERE id = {id}")
+
+    @staticmethod
+    def read_all() -> list[tuple]:
+        return db.select(f"SELECT * FROM specialties")
+
+    @staticmethod
+    def delete(id):
+        db.execute(f"DELETE FROM specialties WHERE id = {id}")
+
+
+class Applications:
+    @staticmethod
+    def create(student_id, speciality_id, point1, point2, point3):
+        values = (student_id, speciality_id, point1, point2, point3)
+        db.execute(f"INSERT INTO applications (student_id, speciality_id, point1, point2, point3) VALUES {values}")
+
+    @staticmethod
+    def read(id) -> tuple:
+        return db.select_one(f"SELECT * FROM applications WHERE rowid = {id}")
+
+    @staticmethod
+    def read_all() -> list[tuple]:
+        return db.select(f"SELECT * FROM applications")
+
+    @staticmethod
+    def delete(id):
+        db.execute(f"DELETE FROM applications WHERE rowid = {id}")
+
+
+
--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from secret import token
 bot = telebot.TeleBot(token)
 
 
+def get_arguments(message) -> tuple[str, ...]:
+    args = message.text.split()[1:]
+    if len(args) == 0:
+        bot.send_message(message.chat.id, "требуются аргументы команды")
+        return
+    return tuple(args)
+
+
 @bot.message_handler(commands=["start"])
 def start_message(message):
     bot.send_message(message.chat.id, "привет")
@@ -27,4 +35,30 @@ def exam_schedule(message):
         txt += f'{i[0]} - {i[1]}\n'
     bot.send_message(message.chat.id, txt)
 
+
+@bot.message_handler(commands=["испытания"])
+def entrance_tests(message):
+    id = int(get_arguments(message)[0])
+    if id is None:
+        return
+
+    speciality = database.Specialties.read(id)
+    txt = f"Вступительные испытания для специальности {speciality[1]} - {', '.join(speciality[2:])}"
+    bot.send_message(message.chat.id, txt)
+
+
+@bot.message_handler(commands=["студент"])
+def student(message):
+    student_id = int(get_arguments(message)[0])
+    txt = 'сумма баллов по каждой специальности:\n'
+    print(student_id)
+
+    for field in [i for i in database.Applications.read_all() if i[0] == student_id]:
+        speciality = database.Specialties.read(field[1])[1]
+        txt += f'{speciality} - {sum(field[2:])}\n'
+
+    bot.send_message(message.chat.id, txt)
+
+
+print('бот запущен')
 bot.infinity_polling()
